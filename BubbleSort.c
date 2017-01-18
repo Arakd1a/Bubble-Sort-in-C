320332bcbc78f301faa9c779372de69ba5d19f29
Matt Barker <matt@barker.space>
2017-01-18 12:41:46 +1000

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,44 +1,63 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
+#include<unistd.h>
+#define MAX 10
+#include <unistd.h>
+#include <stdio.h>
+#include<stdio.h>
+#include<dos.h>
 
-int main() {
+main() {
 
-    int i, temp, sorted;
-    int numbersListSize = 10;
-    int numbersList [numbersListSize];
+    int i, temp,sorted,passes;
+    int size = 300;
+    int array[size];
 
-    for(i=0; i<numbersListSize; i++) {
-        numbersList[i] = ( rand()%25)+1;
+    for(i=0; i<size; i++) {
+        array[i] = (rand()%100);
+
     }
+    printf("[UNSORTED LIST]\n");
+    for(i=0; i<size; i++) {
+        printf("%d, ",array[i]);
 
-    printf("UNSORTED\n");
-    for(i=0; i<numbersListSize; i++) {
-        printf(" %d \n", numbersList[i]);
     }
-
+    printf("\n\n");
+    passes = 0;
 
     while(1) {
         sorted = 0;
-        for(i=0; i<numbersListSize-1; i++) {
-            if(numbersList[i] >numbersList[i+1]) {
-                int temp = numbersList[i];
-                numbersList[i] =   numbersList[i+1];
-                numbersList[i+1] =   temp;
+        for(i=0; i<size-1; i++) {
+
+            if(array[i]>array[i+1]) {
+                printf("[%d,%d]\t\t%d > %d\t\t"
+                       ,array[i],array[i+1],array[i],array[i+1]);
+                temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+                passes++;
                 sorted = 1;
+
+                printf(" [%d,%d]\n\n",array[i],array[i+1]);
+
             }
         }
-        if(sorted==0) {
+        if(sorted ==0 ) {
             break;
         }
     }
-    printf("\n\n");
-    printf("SORTED LIST\n");
-    for(i=0; i<numbersListSize; i++) {
-        printf(" %d \n", numbersList[i]);
+
+
+    printf("\nSort completed with [%d] passes.\n\n\n",passes);
+    printf("[SORTED LIST]\n");
+
+    for(i=0; i<size; i++) {
+        printf("%d, ",array[i]);
+
+
+
     }
 
-
+      printf("\n\n");
     return 0;
-
-
 }
